38951ddc176b2ede999eb8d41b703f794f796eb0
schlaBAM <brett.morrison@shopify.com>
2020-03-24 18:32:55 -0700

    Initial skeleton (app layout + component config)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-// let's go!
-alert('yooo');
+import React from 'react'
+import { render } from 'react-dom'
+import App from "./components/App";
+import './css/style.css'
+
+render(<App />, document.querySelector('#main'));
