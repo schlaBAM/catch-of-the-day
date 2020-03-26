3fd2a87f33168d06cc54b08ff5bd1178797bad1c
schlaBAM <brett.morrison@shopify.com>
2020-03-26 00:22:14 -0700

    Generate sampleFishes to make testing easier, created Fish component for menu

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from "./Header";
 import Inventory from "./Inventory"
 import Order from "./Order"
+import sampleFishes from '../sample-fishes'
+import Fish from "./Fish";
 
 class App extends React.Component {
     state = {
@@ -15,14 +17,24 @@ class App extends React.Component {
         this.setState({fishes});
     };
 
+    loadSampleFishes = () => {
+        this.setState({fishes : sampleFishes});
+    };
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline = 'This shit is amazing' />
+                    <ul className='fishes'>
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+                    </ul>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish}/>
+                <Inventory
+                    addFish={this.addFish}
+                    loadSampleFishes = {this.loadSampleFishes}
+                />
             </div>
         )
     }
