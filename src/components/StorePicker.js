72aa5d94f887e933b5280c36296b4d3f92dbd83c
schlaBAM <brett.morrison@shopify.com>
2020-03-25 17:46:51 -0700

    Added redirect from StorePicker to include storeName in the URL using a reference from the input

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,18 +1,28 @@
 import React from 'react'
-import { getFunName } from "../helpers";
+import {getFunName} from "../helpers";
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = event => {
+        event.preventDefault(); //Stop form from submitting
+        const storeName = this.myInput.current.value; //text from the input
+        this.props.history.push(`/store/${storeName}`)
+    };
+
     render() {
         return (
-            <React.Fragment>
-                { /* heyooo comment */ }
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+                <input type="text"
+                       ref={this.myInput}
+                       required
+                       placeholder="Store Name"
+                       defaultValue={getFunName()}
+                />
                 <button type="submit">Visit Store</button>
             </form>
-            </React.Fragment>
-    )
+        )
     }
 }
 
